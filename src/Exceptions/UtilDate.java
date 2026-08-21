f909d9f70d22c49eb3ea7a70e5795b83e74f35b7
nwvei <aaronvillegasl04@gmail.com>
2026-08-21 13:07:25 -0600

    Cambios en UtilDate
    Cambios en UtilDate sobre algunas funciones,

--- src/Exceptions/UtilDate.java
+++ src/Exceptions/UtilDate.java
@@ -5,6 +5,7 @@
 package Exceptions;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
 /**
@@ -17,5 +18,41 @@ public class UtilDate {
     }
     public static LocalDate toLocalDate(String date){
         return LocalDate.parse(date,DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+
+    public static boolean esFechaFutura(LocalDate fecha) {
+        return fecha != null && fecha.isAfter(LocalDate.now());
+    }
+
+    public static boolean esRangoValido(LocalDate inicio, LocalDate fin) {
+        return inicio != null && fin != null && fin.isAfter(inicio);
+    }
+
+    public static int calcularDias(LocalDate inicio, LocalDate fin) {
+    int dias = 0;
+    LocalDate fecha = inicio;
+
+    while (fecha.isBefore(fin)) {
+        dias++;
+        fecha = fecha.plusDays(1);
+    }
+
+    return dias;
+}
+
+    public static int calcularPeriodos(long dias) {
+        int periodos = (int) (dias / 30);
+        if (dias % 30 != 0) {
+            periodos++;
+        }
+        return periodos;
+    }
+    
+    public static int calcularEdad(LocalDate fechaNacimiento) {
+        return Period.between(fechaNacimiento, LocalDate.now()).getYears();
+    }
+    
+    public static boolean seSolapan(LocalDate inicioA, LocalDate finA, LocalDate inicioB, LocalDate finB) {
+        return !inicioA.isAfter(finB) && !inicioB.isAfter(finA);
     }
 }
